50c5a690ac674fa3fd29fbe24489ec77fc6c7e6c
David Krieger <davidjkrieger@gmail.com>
2019-01-27 15:41:02 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,7 @@
+var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
+
+var element = "mars bar";
+function addElementToBeginningOfArray(chocolateBars, element){
+  return chocolateBars.shift(chocolateBars, element);
+}
+function destructivelyAddElementToBeginningOfArray()[]
